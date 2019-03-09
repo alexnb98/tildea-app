db5a729514881bb502a129f3c5118513dc9c8daa
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-03-09 09:40:10 +0100

    added game3 starter template

---
+++ frontend/src/pages/Game3.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Game3 extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Helluo from game 3</h1>
+            </div>
+        )
+    }
+}
+
+export default Game3;
