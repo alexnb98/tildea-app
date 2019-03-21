28aad6df32d7470df272456c58e822ad84b77619
Alexander Becker <alexandernino98@gmail.com>
2019-03-21 15:58:31 +0100

    code review alexander

--- frontend/src/components/Option.js
+++ frontend/src/components/Option.js
@@ -1,26 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const Option = props => (
-        <button onClick={props.click}
-        // className="px-5 py-3 display-4 rounded shadow"
-        className="display-4 border-0 rounded px-5 py-3 btn-light shadow"
-        disabled={props.disabled}
-        >
-        {props.option}
-    </button>
+const Option = (props) => (
+	<button
+		onClick={props.click}
+		className="display-4 border-0 rounded px-5 py-3 btn-light shadow"
+		disabled={props.disabled}
+	>
+		{props.option}
+	</button>
 );
 
 export default Option;
-// import React from "react";
-
-// const Option = props => (
-//     <div
-//         onClick={props.click}
-//         className="px-5 py-3 display-4 rounded shadow"
-//     >
-//         {props.option}
-//     </div>
-// );
-
-// export default Option;
-
