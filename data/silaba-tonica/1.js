ba250a9e63941edb70fe46b9f662dd644e69c6ec
Alexander <alexandernino98@gmail.com>
2019-06-07 17:20:36 +0200

    backend, routes and exercises with express

---
+++ data/silaba-tonica/1.js
@@ -0,0 +1,15 @@
+module.exports = {
+    game: 1,
+    exercises: [
+        {word: ["Pre", "gun", "ta"], correct: 1},
+        {word: ["A", "mis", "tad"], correct: 2},
+        {word: ["Ar", "pa"], correct: 0},
+        {word: ["Bi", "blio", "te", "ca"], correct: 2},
+        {word: ["Bi", "lle", "te"], correct: 1},
+        {word: ["Ca", "be", "llo"], correct: 1},
+        {word: ["Ca", "lle"], correct: 0},
+        {word: ["Ca", "mi", "se", "ta"], correct: 2},
+        {word: ["Can", "ti", "na"], correct: 1},
+        {word: ["Ca", "ra", "col"], correct: 2}
+    ]
+};
