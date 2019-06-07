b08b8572ce5ce87aef3a6899bb62a651b66c557a
Alexander <alexandernino98@gmail.com>
2019-06-07 17:22:44 +0200

    word -> options

--- data/silaba-tonica/1.js
+++ data/silaba-tonica/1.js
@@ -1,15 +1,15 @@
 module.exports = {
     game: 1,
     exercises: [
-        {word: ["Pre", "gun", "ta"], correct: 1},
-        {word: ["A", "mis", "tad"], correct: 2},
-        {word: ["Ar", "pa"], correct: 0},
-        {word: ["Bi", "blio", "te", "ca"], correct: 2},
-        {word: ["Bi", "lle", "te"], correct: 1},
-        {word: ["Ca", "be", "llo"], correct: 1},
-        {word: ["Ca", "lle"], correct: 0},
-        {word: ["Ca", "mi", "se", "ta"], correct: 2},
-        {word: ["Can", "ti", "na"], correct: 1},
-        {word: ["Ca", "ra", "col"], correct: 2}
+        {options: ["Pre", "gun", "ta"], correct: 1},
+        {options: ["A", "mis", "tad"], correct: 2},
+        {options: ["Ar", "pa"], correct: 0},
+        {options: ["Bi", "blio", "te", "ca"], correct: 2},
+        {options: ["Bi", "lle", "te"], correct: 1},
+        {options: ["Ca", "be", "llo"], correct: 1},
+        {options: ["Ca", "lle"], correct: 0},
+        {options: ["Ca", "mi", "se", "ta"], correct: 2},
+        {options: ["Can", "ti", "na"], correct: 1},
+        {options: ["Ca", "ra", "col"], correct: 2}
     ]
 };
